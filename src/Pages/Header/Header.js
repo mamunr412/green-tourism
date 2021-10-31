74da13b402295d174f16ab7f2f39f75b3803058c
mimoon5860 <mahmodulislammoon@gmail.com>
2021-10-31 22:54:49 +0600

    little change on nav bar

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -10,13 +10,11 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand className="border-bottom pb-0" style={{ fontFamily: "'Lobster', cursive", fontSize: '30px' }} as={Link} to="/">Green Tourism</Navbar.Brand>
+                <Navbar.Brand className="border-bottom pt-0" style={{ fontFamily: "'Lobster', cursive", fontSize: '30px' }} as={Link} to="/">Green Tourism</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to='/home'>Home</Nav.Link>
-                        <Nav.Link as={Link} to='/managetours'>Manage All Joined Tours</Nav.Link>
-                        <Nav.Link as={Link} to='/managetour'>Add or Delete Tour</Nav.Link>
                     </Nav>
                     <Nav>
                         <div>
@@ -25,6 +23,8 @@ const Header = () => {
                                 user.email ?
                                     <div className='d-flex align-items-md-center flex-md-row flex-column pt-3 pt-md-0 border-md-top'>
                                         <p className='text-warning mb-0'>Hello, {user.displayName}</p>
+                                        <Nav.Link as={Link} to='/managetours'>Manage All Joined Tours</Nav.Link>
+                                        <Nav.Link as={Link} to='/managetour'>Add or Delete Tour</Nav.Link>
                                         <Nav.Link as={Link} to='/mytours'>My Tours</Nav.Link>
                                         <Nav.Link onClick={logOut}><i className="fas fa-sign-out-alt"></i> Logout</Nav.Link>
                                     </div>
